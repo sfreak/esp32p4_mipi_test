24e4102a49a2729999e21c4cec7362c05d3488c0
Sebastian Brückner <sb@thebackend.de>
2026-05-25 08:37:59 +0200

    made image resolution dynamically adjust to detected sensor

--- main/main.c
+++ main/main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
-
 #include "esp_err.h"
 #include "esp_log.h"
-
 #include "esp_ldo_regulator.h"
 #include "esp_cam_ctlr.h"
 #include "esp_cam_ctlr_types.h"
 #include "esp_cam_ctlr_csi.h"
 #include "esp_cam_sensor.h"
-
 #include "driver/i2c_master.h"
 #include "esp_sccb_intf.h"
 #include "esp_sccb_i2c.h"
-
+#include "esp_cam_sensor_detect.h"
 #include "driver/isp.h"
-
-//#include "ov9281.h"
-#include "vd56g3.h"
+#include "esp_intr_types.h"
+#include "esp_intr_alloc.h"
 
 #include "csi_lowlevel.h"
 
@@ -29,31 +25,20 @@
 
 #define SENSOR_SCCB_FREQ            100000
 
-#ifdef OV9281_SCCB_ADDR
+// FIXME: dynamically adjust frame sizes to match detected image sensor
+// this will only work for OV9281:
 #define SENSOR_DEFAULT_FORMAT_NAME  "MIPI_2lane_24Minput_RAW8_640x400_100fps"
-#define SENSOR_SCCB_ADDR            OV9281_SCCB_ADDR
-#define SENSOR_BYTES_PER_PIXEL      1
-#define MIPI_CSI_DISP_HSIZE         640
-#define MIPI_CSI_DISP_VSIZE         400
-#define MIPI_CSI_LANE_BITRATE_MBPS  400
-#define sensor_detect_function(x)   ov9281_detect(x)
-#elif defined(VD56G3_SCCB_ADDR)
-// VD56G3
-#define SENSOR_DEFAULT_FORMAT_NAME  "MIPI_2lane_12Minput_RAW8_480x640_88fps"
-#define SENSOR_SCCB_ADDR            VD56G3_SCCB_ADDR
-#define SENSOR_BYTES_PER_PIXEL      1
-#define MIPI_CSI_DISP_HSIZE         480
-#define MIPI_CSI_DISP_VSIZE         640
-#define MIPI_CSI_LANE_BITRATE_MBPS  400
-#define sensor_detect_function(x)   vd56g3_detect(x)
-#else
-#error "No image sensor driver included"
-#endif
+
 
 static const char *TAG = "main";
 
+typedef struct {
+    i2c_master_bus_handle_t i2c_bus_handle;
+    esp_sccb_io_handle_t sccb_handle;
+    esp_cam_sensor_format_t *cam_cur_fmt;
+} sensor_config_t;
 
-esp_err_t sensor_init(void)
+esp_err_t sensor_init(sensor_config_t *out_cfg)
 {
     esp_err_t ret = ESP_FAIL;
 
@@ -74,16 +59,29 @@ esp_err_t sensor_init(void)
         .xclk_pin = -1,
     };
 
-    //---------------SCCB Init------------------//
-    sccb_i2c_config_t i2c_config = {
-        .scl_speed_hz = SENSOR_SCCB_FREQ,
-        .device_address = SENSOR_SCCB_ADDR,
-        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-    };
-    ESP_ERROR_CHECK(sccb_new_i2c_io(i2c_bus_handle, &i2c_config, &cam_config.sccb_handle));
+    //esp_cam_sensor_device_t *cam = NULL;
+    //cam = sensor_detect_function(&cam_config);
 
+    // loop through all enabled image sensor's detect functions
     esp_cam_sensor_device_t *cam = NULL;
-    cam = sensor_detect_function(&cam_config);
+    for (esp_cam_sensor_detect_fn_t *p = &__esp_cam_sensor_detect_fn_array_start; 
+        p < &__esp_cam_sensor_detect_fn_array_end; 
+        ++p) {
+        sccb_i2c_config_t i2c_config = {
+            .scl_speed_hz = SENSOR_SCCB_FREQ,
+            .device_address = p->sccb_addr,
+            .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        };
+        ESP_ERROR_CHECK(sccb_new_i2c_io(i2c_bus_handle, &i2c_config, &cam_config.sccb_handle));
+
+        cam_config.sensor_port = p->port;
+
+        ESP_LOGI(TAG, "sccp addr 0x%02x, sensor_detect fct 0x%08x", p->sccb_addr, p->detect);
+
+        cam = (*(p->detect))(&cam_config);
+        if (cam) break;
+        ESP_ERROR_CHECK(esp_sccb_del_i2c_io(cam_config.sccb_handle));
+    }
 
     if (!cam) {
         ESP_LOGE(TAG, "failed to detect camera sensor");
@@ -123,6 +121,10 @@ esp_err_t sensor_init(void)
     }
     ESP_ERROR_CHECK(ret);
 
+    out_cfg->i2c_bus_handle = i2c_bus_handle;
+    out_cfg->sccb_handle = cam_config.sccb_handle;
+    out_cfg->cam_cur_fmt = cam_cur_fmt;
+
     return ret;
 }
 
@@ -144,6 +146,12 @@ bool mipi_on_trans_finished(esp_cam_ctlr_handle_t handle, esp_cam_ctlr_trans_t *
     return false;
 }
 
+void isr_isp(void *arg)
+{}
+
+void isr_csi_error(void *arg)
+{}
+
 void app_main(void)
 {
 
@@ -155,24 +163,44 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(esp_ldo_acquire_channel(&ldo_mipi_phy_config, &ldo_mipi_phy));
 
-    sensor_init();
+    sensor_config_t sensor_cfg = {0};
+    ESP_ERROR_CHECK(sensor_init(&sensor_cfg));
 
+    cam_ctlr_color_t csi_color = 0;
+    isp_color_t isp_color = 0;
+    float bytes_per_pixel = 0;
+    switch(sensor_cfg.cam_cur_fmt->format) {
+    case ESP_CAM_SENSOR_PIXFORMAT_RAW8:
+        csi_color = CAM_CTLR_COLOR_RAW8;
+        isp_color = ISP_COLOR_RAW8;
+        bytes_per_pixel = 1.0;
+        break;
+    case ESP_CAM_SENSOR_PIXFORMAT_RAW10:
+        csi_color = CAM_CTLR_COLOR_RAW10;
+        isp_color = ISP_COLOR_RAW10;
+        bytes_per_pixel = 5/4;
+        break;
+    default:
+        ESP_LOGE(TAG, "unsupported pixel format");
+        assert(0);
+        break;
+    }
     esp_cam_ctlr_csi_config_t csi_config = {
         .ctlr_id = 0,
-        .h_res = MIPI_CSI_DISP_HSIZE,
-        .v_res = MIPI_CSI_DISP_VSIZE,
-        .lane_bit_rate_mbps = MIPI_CSI_LANE_BITRATE_MBPS,
-        .input_data_color_type = CAM_CTLR_COLOR_RAW8,
-        .output_data_color_type = CAM_CTLR_COLOR_RAW8,
-        .data_lane_num = 2,
+        .h_res = sensor_cfg.cam_cur_fmt->width,
+        .v_res = sensor_cfg.cam_cur_fmt->height,
+        .lane_bit_rate_mbps = sensor_cfg.cam_cur_fmt->mipi_info.mipi_clk / 1000 / 1000 * 2,
+        .input_data_color_type = csi_color,
+        .output_data_color_type = csi_color,
+        .data_lane_num = sensor_cfg.cam_cur_fmt->mipi_info.lane_num,
         .byte_swap_en = false,
-        .queue_items = 1,
+        .queue_items = 2,
     };
     esp_cam_ctlr_handle_t handle = NULL;
     
     void *frame_buffer = NULL;
     size_t frame_buffer_size = 0;
-    frame_buffer_size = MIPI_CSI_DISP_HSIZE * MIPI_CSI_DISP_VSIZE * SENSOR_BYTES_PER_PIXEL;
+    frame_buffer_size = sensor_cfg.cam_cur_fmt->width * sensor_cfg.cam_cur_fmt->height * bytes_per_pixel;
     frame_buffer = heap_caps_calloc(1, frame_buffer_size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT | MALLOC_CAP_DMA);
     if(!frame_buffer)
     {
@@ -180,7 +208,8 @@ void app_main(void)
         return;
     }
 
-    ESP_LOGD(TAG, "HSIZE: %d, VSIZE: %d, bits per pixel: %d", MIPI_CSI_DISP_HSIZE, MIPI_CSI_DISP_VSIZE, SENSOR_BYTES_PER_PIXEL);
+    ESP_LOGD(TAG, "HSIZE: %d, VSIZE: %d, bits per pixel: %d", 
+        sensor_cfg.cam_cur_fmt->width, sensor_cfg.cam_cur_fmt->height, bytes_per_pixel);
     ESP_LOGD(TAG, "frame_buffer_size: %zu", frame_buffer_size);
     ESP_LOGD(TAG, "frame_buffer: %p", frame_buffer);
 
@@ -204,12 +233,12 @@ void app_main(void)
     esp_isp_processor_cfg_t isp_config = {
         .clk_hz = 80 * 1000 * 1000,
         .input_data_source = ISP_INPUT_DATA_SOURCE_CSI,
-        .input_data_color_type = ISP_COLOR_RAW8,
-        .output_data_color_type = ISP_COLOR_RAW8,
+        .input_data_color_type = isp_color,
+        .output_data_color_type = isp_color,
         .has_line_start_packet = false,
         .has_line_end_packet = false,
-        .h_res = MIPI_CSI_DISP_HSIZE,
-        .v_res = MIPI_CSI_DISP_VSIZE,
+        .h_res = sensor_cfg.cam_cur_fmt->width,
+        .v_res = sensor_cfg.cam_cur_fmt->height,
         .flags.bypass_isp = false,
     };
     ESP_ERROR_CHECK(esp_isp_new_processor(&isp_config, &isp_proc));
